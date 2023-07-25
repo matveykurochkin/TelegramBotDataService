037407ba2a91f98bcbbaa42167f82a20ec61103c
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-07-25 14:36:41 +0300

    create class APIController

---
+++ TelegramBotDataService/Controllers/APIController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace TelegramBotDataService.Controllers;
+
+[Route("api")]
+[ApiController]
+// ReSharper disable once InconsistentNaming
+public class APIController : ControllerBase
+{
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    
+    [HttpPost("GetLogFile")]
+    public Task<IActionResult> GetLogFile(DateTime date, CancellationToken cancellationToken)
+    {
+        try
+        {
+
+            return Task.FromResult<IActionResult>(Ok());
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, "Error Get log file");
+            return Task.FromResult<IActionResult>(StatusCode(500, $"An error occurred: {ex.Message}"));
+        }
+    }
+}
