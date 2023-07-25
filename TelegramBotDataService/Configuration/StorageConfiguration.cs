34fb744df34c94b1efe1c5cfe2acd5419884a6df
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-07-25 15:58:20 +0300

    create configuration

---
+++ TelegramBotDataService/Configuration/StorageConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace TelegramBotDataService.Configuration;
+
+public class StorageConfiguration
+{
+    public FileStorageConfiguration? Directory { get; init; }
+}
+
+public class FileStorageConfiguration
+{
+    public string? PathDirectory { get; init; }
+}
