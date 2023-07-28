e80dbe26394e9547dc725072dc1bd66e124862e9
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-07-28 15:29:56 +0300

    methods used in several classes have been created

---
+++ TelegramBotDataService/Methods/MethodListAvailableLogFileByDate.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using NLog;
+
+namespace TelegramBotDataService.Methods;
+
+internal class MethodListAvailableLogFileByDate
+{
+    private static readonly NLog.ILogger Logger = LogManager.GetCurrentClassLogger();
+    public Task<List<string>> ListAvailableLogFileByDate(string pathDirectory, DateTime dateFrom, DateTime dateTo, CancellationToken cancellationToken)
+    {
+        Logger.Info("Start method: {0}", nameof(ListAvailableLogFileByDate));
+
+        List<string> listLogFiles = new();
+
+        if (Directory.Exists(pathDirectory))
+        {
+            var files = Directory.GetFiles(pathDirectory);
+
+            listLogFiles = files
+                .Select(file => Regex.Match(file, @"\d{4}-\d{2}-\d{2}").Value)
+                .Where(date =>
+                {
+                    if (DateTime.TryParse(date, out var currentDate))
+                        return currentDate >= dateFrom && currentDate <= dateTo;
+                    return false;
+                })
+                .ToList();
+
+            Logger.Info("Files in the specified range are found {0}", pathDirectory);
+            return Task.FromResult(listLogFiles);
+        }
+
+        Logger.Info("Files not exist {0}", pathDirectory);
+        return Task.FromResult(listLogFiles);
+    }
+}
