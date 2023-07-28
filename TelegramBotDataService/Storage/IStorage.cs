31309ff019b619f9c164cc62e3258ace4d2eb308
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-07-28 15:28:48 +0300

    create interface IStorage

---
+++ TelegramBotDataService/Storage/IStorage.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+using NLog;
+
+namespace TelegramBotDataService.Storage;
+
+internal interface IStorage
+{
+    public Task<Stream?> GetLogFileByDate(DateTime date, CancellationToken cancellationToken);
+    public Task<List<string>> GetListAvailableLogFile(CancellationToken cancellationToken);
+    public Task<List<string>> GetListAvailableLogFileByDate(DateTime dateFrom, DateTime dateTo, CancellationToken cancellationToken);
+    
+}
