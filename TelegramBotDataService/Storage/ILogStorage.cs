7248cdd61bcbe64cf93e45e6b751f514166dcb29
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-07-28 16:58:06 +0300

    create comments

---
+++ TelegramBotDataService/Storage/ILogStorage.cs
@@ -0,0 +1,28 @@
+﻿namespace TelegramBotDataService.Storage;
+
+internal interface ILogStorage
+{
+    /// <summary>
+    /// Метод для получения log-файла по дате 
+    /// </summary>
+    /// <param name="date">дата в формате yyyy-MM-dd</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<Stream?> GetByDate(DateTime date, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Метод для получения списка доступных сервисных log-файлов
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<List<string>> GetListAvailable(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Метод возвращающий список доступных сервисных log-файлов, лежащих в промежутке указанных дат
+    /// </summary>
+    /// <param name="dateFrom">дата в формате yyyy-MM-dd</param>
+    /// <param name="dateTo">дата в формате yyyy-MM-dd</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<List<string>> GetListAvailableByDate(DateTime dateFrom, DateTime dateTo, CancellationToken cancellationToken);
+}
